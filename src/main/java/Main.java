01a49ad1597724d2e6cdc737240102767f8ec420
Limpsobaka <limpsobaka@yandex.ru>
2022-10-16 16:37:43 +0300

    Change Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,19 +11,25 @@ import java.util.List;
 
 public class Main {
   public static ObjectMapper mapper = new ObjectMapper();
-  public static void main(String[] args) throws IOException {
-    CloseableHttpClient httpClient = HttpClients.createDefault();
-    HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
-    request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
-    CloseableHttpResponse response = httpClient.execute(request);
+  public static void main(String[] args) {
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+      request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+      CloseableHttpResponse response = httpClient.execute(request);
 
-    List<Cat> cats = mapper.readValue(
-            response.getEntity().getContent().readAllBytes(),
-            new TypeReference<>() {
-            }
-    );
-    cats.stream()
-            .filter(cat -> cat.getUpvotes() > 0)
-            .forEach(System.out::println);
+      List<Cat> cats = mapper.readValue(
+              response.getEntity().getContent().readAllBytes(),
+              new TypeReference<>() {
+              }
+      );
+
+      response.close();
+
+      cats.stream()
+              .filter(cat -> cat.getUpvotes() > 0)
+              .forEach(System.out::println);
+    } catch (IOException e) {
+      System.out.println("Произошла ошибка: " + e.getMessage());
+    }
   }
 }
